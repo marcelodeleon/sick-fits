a11b1d81ba83c029bd2e6400daf905dcf88ebdb1
marcelodeleon <mdeleoncosentino@gmail.com>
2018-11-27 03:40:48 -0300

    Video 17 >> Create Items using Mutations

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,10 +1,8 @@
-import React from 'react';
-import Link from 'next/link';
+import CreateItem from '../components/CreateItem';
 
 const Sell = props => (
   <div>
-    <p>Sell!</p>
-    <Link><a href='/'>Home</a></Link>
+    <CreateItem />
   </div>
 );
 

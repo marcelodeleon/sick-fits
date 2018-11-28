5ad5b273896d6615d5678fd3b7165fdbc1c20233
marcelodeleon <mdeleoncosentino@gmail.com>
2018-11-28 01:09:05 -0300

    Fixed annoying issue with item listing presentation

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -23,7 +23,7 @@ const Center = styled.div`
 
 const ItemsList = styled.div`
   display: grid;
-  grid-template-column: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
   grid-gap: 60px;
   max-width: ${props => props.theme.maxWidth};
   margin: 0 auto;

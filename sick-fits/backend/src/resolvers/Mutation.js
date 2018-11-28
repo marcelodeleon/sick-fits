7c5e62f1b0b8a2a03ed98f9fc31ef757485dca04
marcelodeleon <mdeleoncosentino@gmail.com>
2018-11-28 13:00:54 -0300

    Video 20 >> Delete Item

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -13,16 +13,28 @@ const Mutations = {
   },
   updateItem(parent, args, ctx, info) {
     // Take a copy of the updates and remove the id.
-    const updates = { ...args }
-    delete updates.id
+    const updates = {...args};
+    delete updates.id;
 
-    return ctx.db.mutation.updateItem({
-      data: updates,
-      where: {
-        id: args.id
-      }
-    }, info);
-  }
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info,
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = {id: args.id};
+
+    // Intermediary query, instead of passing info we pass
+    // raw GraphQL to specify what is returned.
+    const item = await ctx.db.query.item({where}, `{id title}`);
+
+    return ctx.db.mutation.deleteItem({where}, info);
+  },
 };
 
 module.exports = Mutations;
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
-
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
 
 class Item extends React.Component {
   static propTypes = {
@@ -38,7 +38,7 @@ class Item extends React.Component {
             <a>Edit</a>
           </Link>
           <button>Add to Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete Item</DeleteItem>
         </div>
       </ItemStyles>
     );

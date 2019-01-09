b999e6235747241a1944a69fabf1b739786d6e98
marcelodeleon <mdeleoncosentino@gmail.com>
2019-01-08 23:49:55 -0300

    Video 24 >> User Signup and Permission Flow

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config({path: 'variables.env'});
 const createServer = require('./createServer');
 const db = require('./db');
+const cookieParser = require('cookie-parser');
 
 const server = createServer();
 
 //TODO Use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
+
 //TODO Use express middleware to populate current user
 
 server.start(

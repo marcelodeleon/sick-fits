a28cee8d4515c96f6d2b987c29a26548a263f111
marcelodeleon <mdeleoncosentino@gmail.com>
2019-01-23 04:42:46 -0300

    Video 28 >> Sign Out Button

---
+++ sick-fits/frontend/components/Signout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import {Mutation} from 'react-apollo';
+import {CURRENT_USER_QUERY} from './User';
+
+const SIGNOUT_MUTATION = gql`
+  mutation SIGNOUT_MUTATION {
+    signout {
+      message
+    }
+  }
+`;
+
+const Signout = props => (
+  <Mutation
+    mutation={SIGNOUT_MUTATION}
+    refetchQueries={[{query: CURRENT_USER_QUERY}]}>
+    {signout => <button onClick={signout}>Signout</button>}
+  </Mutation>
+);
+
+export default Signout;
